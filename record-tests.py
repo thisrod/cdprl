fcffd52659b5916eba9487708b2c61c9e972286e
Rodney Polkinghorne <thisrod@gmail.com>
2011-08-25 14:38:18 +1000

    Changed test cases for new Record interface.

--- record-tests.py
+++ record-tests.py
@@ -9,33 +9,33 @@ class TestIdentical(TestCase):
 	"The average of identical data is the datum."
 
 	def setUp(self):
-		self.specimen = Record(1)
+		self.specimen = Record(timestep = 1)
 		
 	def testOneSample(self):
-		self.specimen.enter(0, 7)
-		self.assertTrue(self.specimen.mean(0) == 7)
+		self.specimen[0] = 7
+		self.assertTrue(self.specimen(0) == 7)
 		
 	def testThreeSamples(self):
-		self.specimen.enter(0, 7, 'run1')
-		self.specimen.enter(0, 7, 'run2')
-		self.specimen.enter(0, 7, 'run3')
-		self.assertTrue(self.specimen.mean(0) == 7)
+		self.specimen[0, 'run1'] = 7
+		self.specimen[0, 'run2'] = 7
+		self.specimen[0, 'run3'] = 7
+		self.assertTrue(self.specimen(0) == 7)
 		
 	def testConstant(self):
-		self.specimen.enter(-1, 7)
-		self.specimen.enter(1, 7)
-		self.assertTrue(self.specimen.mean(0) == 7)
+		self.specimen[-1] = 7
+		self.specimen[1] = 7
+		self.assertTrue(self.specimen(0) == 7)
 		
 	def testInterpolation(self):
-		self.specimen.enter(-0.1, 7)
-		self.specimen.enter(0.1, 7)
-		self.assertTrue(self.specimen.mean(0) == 7)
-		
+		self.specimen[-0.1] = 7
+		self.specimen[0.1] = 7
+		self.assertTrue(self.specimen(0) == 7)
+
 
 class TestTiming(TestCase):
 	
 	def setUp(self):
-		self.specimen = Record(1)
+		self.specimen = Record(timestep = 1)
 		
 	def testAfter(self):
 		for t in 1e-3, 1, 1000:
