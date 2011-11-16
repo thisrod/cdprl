38abb3485a7e76f8aebd131620e9bba5d747ce31
Rodney Polkinghorne <thisrod@gmail.com>
2011-11-16 16:11:15 +1100

    Vectorised weightings and added increments.

--- weightings.py
+++ weightings.py
@@ -1,37 +1,58 @@
 from namespace import *
 
-class Weighting:
+class Weightings(object):
+
+	"""Stores an ndarray of values, and one of weights.  The weights correspond to slices of the values along the leading dimension."""
 	
-	def __init__(self, value, weight = 1):
-		self.mean = value
-		self.weight = weight
+	def __init__(self, values, weights = None):
+		assert values.shape[0:1] == weights.shape
+		self.means = values
+		self.weights = weights
 		
 	def __repr__(self):
-		return "Weighting(%s, weight = %f)" % (repr(self.mean), self.weight)
+		return "Weightings(%s, weights = %s)" % (repr(self.means), repr(self.weights))
 		
 	def __eq__(self, other):
-		return self.mean == other.mean and self.weight == other.weight
+		# We care about permutations because of elemental arithmetic
+		return self.means == other.means and self.weights == other.weights
 		
 	def __add__(self, other):
-		return Weighting(self.mean + other.mean, self.weight + other.weight)
+		return Weighting(self.means + other.means, self.weights + other.weights)
 		
 	def __mul__(self, scalar):
-		return Weighting(self.mean*scalar, self.weight*scalar)	
+		return Weighting(self.means*scalar, self.weights*scalar)	
 		
 	def __rmul__(self, scalar):
-		return Weighting(scalar*self.mean, scalar*self.weight)	
+		return Weighting(scalar*self.means, scalar*self.weights)	
 			
 	def __div__(self, scalar):
-		return Weighting(self.mean/scalar, self.weight/scalar)
+		return Weighting(self.means/scalar, self.weights/scalar)
 		
 	def combine(self, other):
-		# FIXME: The next two lines show why it's better to store mean*weight than mean (except that it adds a round-trip problem).  Maybe the correct answer is to store the value on construction, then coerce to a total if combined with a different mean and non-zero weight.  This could be done as three subclasses: ZeroWeighting, SingleValueWeighting, CombinedValueWeighting
-		if self.weight == 0: return other
-		if other.weight == 0: return self
-		weight = self.weight + other.weight
-		mean = (self.mean*self.weight + other.mean*other.weight) / weight
-		return Weighting(mean, weight)
+		# Fails on zero weights.  Note the argument at http://alvyray.com/Memos/MemosCG.htm#ImageCompositing, re alpha channels.  Zero weight implies the value doesn't matter, so the round-trip error is spurious.
+		broadcast = weights.shape + (len(self.means.shape)-1) * (1,)
+		ws = self.weights.copy().reshape(broadcast)
+		wo = other.weights.copy().reshape(broadcast)
+		weights = ws + wo
+		means = (self.means*ws + other.means*wo) / weights
+		return Weighting(means, weights.reshape(self.weights))
+		
+
+class WeightingIncrement(object):
+	
+	"""An increment to a Weighting, stored as an absolute change to the mean, and a relative change to the weight.  When called with a Weighting as argument, it returns an Weighting to be added."""
+	
+	def __init__(self, values, weights):
+		assert values.shape[0:1] == weights.shape
+		self.means = values
+		self.weights = weights
+		
+	def __eq__(self, other):
+		return self.means == other.means and self.weights == other.weights
+		
+	def __call__(self, ws):
+		return Weightings(self.means + ws.means, (1+self.weights)*ws.weights)
 		
 		
 def weightclose(self, other):
-	return allclose(self.mean, other.mean) and allclose(self.weight, other.weight)
+	return allclose(self.means, other.means) and allclose(self.weights, other.weights)
